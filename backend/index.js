7bcc0f0068cb8dbcf217229ca4f2df83ed128b56
MatHyp <matijoker29@gmail.com>
2023-03-19 12:25:00 +0100

    Added jsonwebtoken and get method for userdata

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,8 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
-
 // CONFIGURATIONS
 
 const __filename = fileURLToPath(import.meta.url);
@@ -46,7 +46,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 // ROUTES
 app.use("/auth", authRoutes);
-
+app.use("/users", userRoutes);
 const PORT = process.env.PORT || 6001;
 
 mongoose
